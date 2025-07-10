2c081d0ae41cb0a114a658412d914756a2cc88b1
unknown <atit22@gmail.com>
2025-07-10 23:54:12 +0530

    adding

--- parent/main.tf
+++ parent/main.tf
@@ -9,3 +9,9 @@ source = "../child/RG"
 resource_group_name = "RG-Atit2"
 location="centralindia"
 }
+
+module "RG" {
+source = "../child/RG"
+resource_group_name = "RG-Atit3"
+location="centralindia"
+}
